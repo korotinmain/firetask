76c3029d08d1019ac5256fd2b5c003b5a7ef6ff7
Thomas Klaner <thomas@apollo.ai>
2021-10-06 16:12:01 +0200

    fix deploy and prod fixtures

--- functions/src/commands/runFixtures.ts
+++ functions/src/commands/runFixtures.ts
@@ -1,12 +1,24 @@
 import * as admin from 'firebase-admin';
+import { readFileSync } from 'fs';
 import { omit } from 'lodash';
 import { tasks } from '../data/tasks';
 import { users } from '../data/users';
 
-// connect to firebase emulators
-process.env.FIREBASE_AUTH_EMULATOR_HOST = 'localhost:4102';
-process.env.FIRESTORE_EMULATOR_HOST = 'localhost:4103';
-const app = admin.initializeApp({ projectId: 'firetasks-local' });
+let adminOptions: any;
+if (process.env.PROD) {
+  const keyFile = 'apollo-fire-tasks-firebase-adminsdk-b8mgo-40040892e6.json';
+  const key = JSON.parse(readFileSync(keyFile).toString());
+  adminOptions = {
+    projectId: key.project_id,
+    credential: admin.credential.cert(key),
+  };
+} else {
+  // connect to firebase emulators
+  process.env.FIREBASE_AUTH_EMULATOR_HOST = 'localhost:4102';
+  process.env.FIRESTORE_EMULATOR_HOST = 'localhost:4103';
+  adminOptions = { projectId: 'firetasks-local' };
+}
+const app = admin.initializeApp(adminOptions);
 
 const auth = app.auth();
 const firestore = app.firestore();
